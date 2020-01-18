6b9a284947b845821c9ee194b30227f1b4c8f088
Nick Boober <nboober@terpmail.umd.edu>
2020-01-18 15:00:18 -0500

    updated the validation for credit cards in the model

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,6 +1,6 @@
 class CreditCard < ApplicationRecord
     belongs_to :user, inverse_of: :credit_card
-    validates :cardnumber, :inclusion => 1..9999999999999999   
+    validates :cardnumber, presence: true  
     validates :expmonth, :inclusion => 1..12   
     validates :expyear, :inclusion => 2019..2999   
     validates :securitycode, :inclusion => 1..999   
